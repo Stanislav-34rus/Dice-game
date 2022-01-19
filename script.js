93513db342f9698de2c36b721e5b5246e1535833
Stanislav-34rus <80258515+Stanislav-34rus@users.noreply.github.com>
2022-01-19 23:01:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 let imags = [
-    src = "img/dice1.png",
-    src = "img/dice2.png",
-    src = "img/dice3.png",
-    src = "img/dice4.png",
-    src = "img/dice5.png",
-    src = "img/dice6.png"
+    src = "https://sun9-46.userapi.com/impg/QL2kaolbcQpHSUL2azsqWcdMfNGWhJrhYBT_aQ/uoFCTKgluLo.jpg?size=557x557&quality=96&sign=3b82ba5826fabbd1464811a98947c150&type=album",
+    src = "https://sun9-58.userapi.com/impg/obE1Bu6OeTy-7IX5K3RK25V9UlfbuPm6XPEApw/TfM4XIXKZBQ.jpg?size=557x557&quality=96&sign=64564e5d1f7d7b6a67decfd00c19b294&type=album",
+    src = "https://sun9-47.userapi.com/impg/J3umU7PUMRcPN2h04bxZD4eiTVMHELYHImKl-A/qd9_SB94LcI.jpg?size=557x557&quality=96&sign=200f558a2f4afae8574b97ecb0c4c22c&type=album",
+    src = "https://sun9-46.userapi.com/impg/BPTJhER2Ncao4L9m99csgyFwWwSAWUF8igXF_Q/8I9IDRix7-M.jpg?size=557x557&quality=96&sign=74f0e24f13c542a69a12b7bdebc7d7f2&type=album",
+    src = "https://sun9-87.userapi.com/impg/fp_DvrNXAXYKQ9Rxxkijdqy1Pf1aAlkIq0QfMQ/DYE_ZFaUvZk.jpg?size=1200x1200&quality=96&sign=7528bedcf08b5ca7b4c3acf3f1611119&type=album",
+    src = "https://sun9-36.userapi.com/impg/cGgLAl2RduDMem5eA9zvbrtWk0tR0wQAQZvXoQ/nYV-SAn7W_Q.jpg?size=557x557&quality=96&sign=857c8565a752adf90ddc04968d33e40a&type=album"
 
 ]
 let imags2 = [
-    src = "img/dice1.png",
-    src = "img/dice2.png",
-    src = "img/dice3.png",
-    src = "img/dice4.png",
-    src = "img/dice5.png",
-    src = "img/dice6.png"
+    src = "https://sun9-46.userapi.com/impg/QL2kaolbcQpHSUL2azsqWcdMfNGWhJrhYBT_aQ/uoFCTKgluLo.jpg?size=557x557&quality=96&sign=3b82ba5826fabbd1464811a98947c150&type=album",
+    src = "https://sun9-58.userapi.com/impg/obE1Bu6OeTy-7IX5K3RK25V9UlfbuPm6XPEApw/TfM4XIXKZBQ.jpg?size=557x557&quality=96&sign=64564e5d1f7d7b6a67decfd00c19b294&type=album",
+    src = "https://sun9-47.userapi.com/impg/J3umU7PUMRcPN2h04bxZD4eiTVMHELYHImKl-A/qd9_SB94LcI.jpg?size=557x557&quality=96&sign=200f558a2f4afae8574b97ecb0c4c22c&type=album",
+    src = "https://sun9-46.userapi.com/impg/BPTJhER2Ncao4L9m99csgyFwWwSAWUF8igXF_Q/8I9IDRix7-M.jpg?size=557x557&quality=96&sign=74f0e24f13c542a69a12b7bdebc7d7f2&type=album",
+    src = "https://sun9-87.userapi.com/impg/fp_DvrNXAXYKQ9Rxxkijdqy1Pf1aAlkIq0QfMQ/DYE_ZFaUvZk.jpg?size=1200x1200&quality=96&sign=7528bedcf08b5ca7b4c3acf3f1611119&type=album",
+    src = "https://sun9-36.userapi.com/impg/cGgLAl2RduDMem5eA9zvbrtWk0tR0wQAQZvXoQ/nYV-SAn7W_Q.jpg?size=557x557&quality=96&sign=857c8565a752adf90ddc04968d33e40a&type=album"
 ]
 
 
